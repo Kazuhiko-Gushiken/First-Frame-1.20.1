f18910bc7d9c52cb174ab142af6acd97d5a605ec
Kazuhiko Gushiken <just.a.stem.nerd@gmail.com>
2023-10-11 17:27:56 -0700

    Initial Commit Failed

---
+++ src/main/java/net/inno/firstframe/FirstFrameClient.java
@@ -0,0 +1,10 @@
+package net.inno.firstframe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstFrameClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
